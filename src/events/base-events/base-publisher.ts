2a9e7f3af06c8a616b7521fea1423c6df687b0ba
muhammedshahinshap <118247312+muhammedshahinshap@users.noreply.github.com>
2023-07-05 10:53:34 +0530

    updates

--- src/events/base-events/base-publisher.ts
+++ src/events/base-events/base-publisher.ts
@@ -10,6 +10,8 @@ export abstract class Publisher<T extends Event> {
     this.client = client;
   }
   async publish(data: T["data"]): Promise<void> {
+    console.log(data);
+    
     return new Promise<void>((res, rej) => {
       this.client.publish(this.subject, JSON.stringify(data));
       return res();
