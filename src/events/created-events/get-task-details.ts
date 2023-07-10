6ee4e6d56d1c6369e3ad144833eb75cddcad2dfb
muhammedshahinshap <118247312+muhammedshahinshap@users.noreply.github.com>
2023-07-10 16:03:56 +0530

    updates

---
+++ src/events/created-events/get-task-details.ts
@@ -0,0 +1,5 @@
+import { Subjects } from "../subjects/Subjects";
+export interface GetTaskDetails {
+  subject: Subjects.getTaskDetails
+  data: any;
+}
--- src/events/subjects/Subjects.ts
+++ src/events/subjects/Subjects.ts
@@ -2,6 +2,5 @@ export enum Subjects {
   getDepartment = "dep&task:getDepartment",
   getDepartmentReply = "dep&task:getDepartmentReply",
   getUserDataNotAssigned = "user:getUserData",
-
-
+  getTaskDetails = "dep&task:getTaskData",
 }
