e648e56d3db6d3a4b5867686ee2f634df97e5ba8
muhammedshahinshap <118247312+muhammedshahinshap@users.noreply.github.com>
2023-07-10 16:13:34 +0530

    updates

---
+++ src/events/created-events/get-task-details-event.ts
@@ -0,0 +1,5 @@
+import { Subjects } from "../subjects/Subjects";
+export interface GetTaskDetailsEvent {
+  subject: Subjects.getTaskDetails;
+  data: any;
+}
--- src/events/created-events/get-task-details.ts
+++
@@ -1,5 +0,0 @@
-import { Subjects } from "../subjects/Subjects";
-export interface GetTaskDetails {
-  subject: Subjects.getTaskDetails
-  data: any;
-}
